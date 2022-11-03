98b31b94f3f4629463b896ec91647cdc122930e7
Hatice-Altun <van.altun@outlook.com>
2022-11-03 18:44:51 +0100

    Conditional Statement Example 3

---
+++ src/com/altun/ConditionalStatement/Main.java
@@ -0,0 +1,26 @@
+package com.altun.ConditionalStatement;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Write a code to get a number from the user and print the whether it is positive or negative.
+    // Test Data - Input number: 35
+    // Expected output: Number is positive
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input number: ");
+        int input = in.nextInt();
+
+        if (input > 0) {
+            System.out.println("Number is positive!");
+        }
+        else if (input < 0) {
+            System.out.println("Number is negative!");
+        }
+        else {
+            System.out.println("Number is 0!");
+        }
+    }
+}
